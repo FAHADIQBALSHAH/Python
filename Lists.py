9cbf03bf29d8b3d2e8e96733f07e6efa27d96e26
Fahad Iqbal Shah <fahadiqbalshah41@gmail.com>
2025-10-02 22:22:11 +0530

    lists in python

---
+++ Lists.py
@@ -0,0 +1,30 @@
+marks = [85.5, 92.3, 77.9, 90.1]
+print(marks)
+print(type(marks))
+
+student = ["FAHAD", 24, 95.5, "SRINAGAR"]
+print(student)
+
+print(student[1])
+print(len(student))
+#print(student[6]) Will give out of range error
+
+student[2] = 100
+print(student)
+
+mylist = [1,2,3,4]
+print(mylist)
+mylist.append(5)
+print(mylist)
+mylist.sort()
+print(mylist)
+mylist.sort(reverse = True)
+print(mylist)
+mylist.reverse()
+print(mylist)
+mylist.insert(0,0)
+print(mylist)
+mylist.remove(0)
+print(mylist)
+mylist.pop(0)
+print(mylist)
