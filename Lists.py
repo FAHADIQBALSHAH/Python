ac4890f50b43bc16bdb803fbec2b6677624ae764
Fahad Iqbal Shah <fahadiqbalshah41@gmail.com>
2025-10-03 15:56:51 +0530

    Questions on Lists

--- Lists.py
+++ Lists.py
@@ -28,3 +28,32 @@ mylist.remove(0)
 print(mylist)
 mylist.pop(0)
 print(mylist)
+
+#Enter three names and store them in a list
+
+name1 = input("Enter 1st Name: ")
+name2 = input("Enter 2nd Name: ")
+name3 = input("Enter 3rd Name: ")
+
+
+list_names =[]
+list_names.append(name1)
+list_names.append(name2)
+list_names.append(name3)
+
+print("List of Names:", list_names)
+
+
+#Check if a list contains a palindrome of elememts
+
+list_num = [1,2,3,2,1]
+print(list_num)
+
+new_list = list_num.copy()
+new_list.reverse()
+print(new_list)
+
+if (list_num == new_list):
+    print("Plaindrome")
+else:
+    print("Not a Palindrome")    
